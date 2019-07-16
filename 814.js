32e673716bcc60506c97e8f3e62851b697ec699a
rancerun <lancesun22@gmail.com>
2019-07-15 22:12:51 -0400

    confused on difference between accepted and not accepted answers

---
+++ 814.js
@@ -0,0 +1,30 @@
+
+// original solution - doesn't work
+var pruneTree = function(root) {
+  if (root.left) {
+    pruneTree(root.left);
+  }
+  if (root.right) {
+    pruneTree(root.right);
+  }
+  if (root.val == 0 && !root.left && !root.right) {
+    root.val = null;
+    return;
+  }
+
+  return root;
+};
+
+// revised solution - works
+var pruneTree = function(root) {
+  if (!root) {
+    return null;
+  }
+  root.left = pruneTree(root.left);
+  root.right = pruneTree(root.right);
+  if (root.val == 0 && !root.left && !root.right) {
+    return null;
+  }
+
+  return root;
+};
