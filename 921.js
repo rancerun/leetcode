db0124872b815532de1431084a5162f4495ba974
rancerun <lancesun22@gmail.com>
2019-07-18 16:43:36 -0400

    completed 921.js

---
+++ 921.js
@@ -0,0 +1,15 @@
+
+var minAddToMakeValid = function(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == ')' && stack[stack.length - 1] == '(') {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  return stack.length;
+};
+
+console.log(minAddToMakeValid('()))(('));
