ff12237c815c84ffc1509bef9b0fa0c3be5c5513
rancerun <lancesun22@gmail.com>
2019-07-19 16:36:57 -0400

    ** did not complete **

---
+++ 861.js
@@ -0,0 +1,20 @@
+// did not complete
+
+var matrixScore = function(a) {
+
+  let numFlip = (num) => {
+    if (num == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  let i = 0;
+
+  let curRow = a[i][j]; // j increases
+  let curCol = a[i][j]; // i increases
+
+};
+
+console.log(matrixScore([[0,0,1,1],[1,0,1,0],[1,1,0,0]]));
